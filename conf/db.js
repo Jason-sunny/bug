9761a8df16ed4f3038db5993a640c63d6286e17f
Jason-sunny <2385585770@qq.com>
2017-03-13 19:43:41 +0800

    添加bug系统

---
+++ conf/db.js
@@ -0,0 +1,16 @@
+/**
+ * @author :  Sunny
+ * @date : 2017/3/12 22:12
+ * @description : MySQL数据库联接配置
+ */
+'use strict';
+
+module.exports = {
+  mysql : {
+    host : '127.0.0.1' ,
+    user : 'root' ,
+    password : 'root' ,
+    database : 'bug' ,
+    port : 3306
+  }
+};
